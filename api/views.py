5d847bd0665473a61bf90dbc191d5324470d5468
shrikanta <shrikantadutta108@gmail.com>
2025-12-23 17:36:42 +0600

    delete data

--- api/views.py
+++ api/views.py
@@ -50,5 +50,18 @@ def student_api(request):
             return HttpResponse(json_data, content_type = 'application/json')
         json_data = JSONRenderer().render(serializer.errors)
         return HttpResponse(json_data, content_type = 'application/json')
+    
+    if request.method == 'DELETE':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata= JSONParser().parse(stream)
+        id = pythondata.get('id')
+        stu = Student.objects.get(id=id)
+        stu.delete()
+        res = {'msg': 'Data Deleted'}
+        json_data = JSONRenderer().render(res)
+        return HttpResponse(json_data, content_type = 'application/json')
+    json_data = JSONRenderer().render(serializer.errors)
+    return HttpResponse(json_data, content_type = 'application/json')
 
        
--- myapp.py
+++ myapp.py
@@ -37,5 +37,13 @@ def update_data():
     r = requests.put(url=URL,data=json_data)
     data = r.json()
     print(data)
-update_data()        
+# update_data()   
+def delete_data():
+    data = { 'id':4}
+
+    json_data = json.dumps(data)
+    r = requests.delete(url=URL,data=json_data)
+    data = r.json()
+    print(data)
+delete_data()       
      
